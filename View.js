4e30495cc0d2fc6ebcffc32d52588b7ae40909f9
emac-E <emackey@wpi.edu>
2023-10-28 18:02:28 -0400

    add landing page html to View

--- View.js
+++ View.js
@@ -1,8 +1,11 @@
 import React from "react";
-import './App.css';
+import './everest_style.css';
 import Create from './Owner/Create.js';
 import Login from './Login.js';
 import About from './About.js';
+
+import logo from './everest.jpg'
+
 
 function ManagerView() {
     return (
@@ -28,10 +31,57 @@ function CustomerView() {
     );
 }
 
-function Landing() {
+export function Landing() {
     return (
         <div className="Landing-page">
-            <h1>Landing Page</h1>
+            <head>
+        <title>Everest Home</title>
+        <meta charset="UTF-8" />
+        <meta name="author" content="Ellen Mackey, John DeLeo" />
+        <meta name="keywords" content="computers, used computers, computer consignment" />
+        <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans" 
+        rel="stylesheet"/>
+      </head>
+      
+      <body>
+      <header>
+	      <p class="logo"><img src={logo} height="150" width="150" align="left"/></p>
+	        <nav class="horizontalNAV">
+	          <ul type="none">
+		          <li><a href="login.html">Sign In</a></li>
+		          <li>Create Store</li>
+		          <li>About Us</li>
+		          <li>Search</li>
+		          <li><a href="FAQ.html">?</a></li>
+	          </ul>
+          </nav>
+          <br clear="right" />
+          <h1>
+	        Everest Computer Consignment
+          </h1>
+    
+          <h2 class="tagline"></h2>
+   
+      </header>
+
+      <div id="elevator">
+
+      <h3>Search thousands of used computers</h3>
+	    <h3>Find rare models and features at low costs</h3>
+	    <h3>New deals every day</h3>
+     
+      </div>
+      </body>
+
+      <br />
+      <hr color="white" clear="right"/>
+      <footer>
+        <address>
+			    Everest Computer Consignment&nbsp;&nbsp;&nbsp;&#9765;&nbsp;&nbsp;&nbsp;
+			    established 2023
+	      </address>
+	
+      </footer>
         </div>
     );
 }
@@ -39,7 +89,7 @@ function Landing() {
 export default function View(props) {
     
     return (
-        <>
+        <div>
             {props.user === 'owner' && <OwnerView />}
             {props.user === 'manager' && <ManagerView />}
             {props.user === 'customer' && <CustomerView />}
@@ -48,6 +98,6 @@ export default function View(props) {
             {props.login === true && <Login handleUser={props.handleUser}/>}
             {props.about === true && <About />}
             
-        </>
+        </div>
     )
 }
