68c47fe4ddecb6e6c273b6a7e3652de44937c427
emac-E <emackey@wpi.edu>
2023-10-30 02:53:19 -0400

    deleting unnecessary files

--- Manager/GenerateReport.js
+++ Manager/GenerateReport.js
@@ -1,6 +1,12 @@
 //Generate Reports
 
 export function GenerateReport() {
+    
+    //by store - search for store and click on store to go to store site
+
+    //by all stores - for each store on the site generate inventory report
+    //with computers listed in ascending or descending order by price
+
     return (
         <div>
             GENERATE INVENTORY REPORT
