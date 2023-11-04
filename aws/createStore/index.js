fe97ea81fd6ff75d28f6f5165e4f052e1e6163a4
John DeLeo <jdeleo@fastmail.com>
2023-11-04 17:13:26 -0400

    Added AWS lamda functions.

---
+++ aws/createStore/index.js
@@ -0,0 +1,77 @@
+const mysql = require('mysql');
+const db_access = require('/opt/nodejs/db_access')
+
+exports.handler = async (event) => {
+  var pool = mysql.createPool({// credentials from db_access layer (loaded separately via console)
+      host: db_access.config.host,
+      user: db_access.config.user,
+      password: db_access.config.password,
+      database: db_access.config.database
+
+  });
+
+  
+  let arg1 = event.arg1;
+  let logo = event.logo;
+  let fname = event.fname;
+  let lname = event.lname;
+  let email = event.email;
+  let password = event.password;
+  let phone = event.phone;
+  let address = event.address;
+  let city = event.city;
+  let state = event.state;
+  let zip = event.zip;
+  let country = event.country;
+  let description = event.description;
+  let response = {};
+  
+  
+  let userQuery = "INSERT INTO Users (userID, userFirst, userLast, userEmail, userStreet, userCity, userState, userZip, userType, password) VALUES ('" + email + "', '" + fname + "', '" + lname + "', '" + email + "', '" + address + "', '" + city + "', '" + state + "', '" + zip + "', 0, '" + password + "');"
+
+  let result = await new Promise((resolve, reject) => {
+      pool.query(userQuery, (err, res) => {
+          if (err) {
+              console.log(err);
+              response = {
+                  statusCode: 400,
+                  body: JSON.stringify('Error: User already exists.'),
+              };
+              reject(response);
+          } else {
+              console.log(res);
+              resolve(res);
+          }
+      });
+  });
+
+  let storeQuery = "INSERT INTO Stores (name, description, image, storeOwner) VALUES ('" + arg1 + "', '" + description + "', '" + logo + "', '" + email + "');";
+
+  result = await new Promise((resolve, reject) => {
+      pool.query(storeQuery, (err, res) => {
+          if (err) {
+              console.log(err);
+              response = {
+                  statusCode: 400,
+                  body: JSON.stringify('Error: Store already exists.'),
+              };
+              reject(response);
+          } else {
+              console.log(res);
+              resolve(res);
+          }
+      });
+  });
+
+
+  response = {
+      statusCode: 200,
+      header: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*"
+      },
+      body: JSON.stringify(result),
+  };
+
+  return response;
+};
---
+++ aws/db_access/nodejs/db_access.js
@@ -0,0 +1,10 @@
+module.exports = {
+    config: {
+        "host": "everestdb.cvr2timum1d3.us-east-1.rds.amazonaws.com",
+        "port": 3306,
+        "user": "admin",
+        "password": "fLi;8E9k*s+=j-r",
+        "database": "consignmentStore",
+    },
+    jwtSecret: "655d4643fb11102f2ec65876e7b41d320a362ef362d4ebee39e0433e342810f9a8a753"
+}
